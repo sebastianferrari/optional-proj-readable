846820ec3a5fb5e25764fe5cbd6a49061770de6b
Sebastian Ferrari <sebastianferrari@live.com>
2018-09-14 23:46:03 -0500

    feat: Arrange categories and posts in main screen

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+const CategoriesSideBar = ({ categories }) => (
+  <div className='col-md-2' style={{ backgroundColor: 'green', color: 'white' }}>
+    Categories
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
-      <h1>Readable App</h1>
+      <div className='container'>
+        <h1>Readable App</h1>
+        <div className='row'>
+          <CategoriesSideBar />
+          <div className='col-md-10' style={{ backgroundColor: 'blue', color: 'white' }}>
+            Posts
+          </div>
+        </div>
+      </div>
     );
   }
 }
